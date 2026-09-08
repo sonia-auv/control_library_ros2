9e9241fb024c68d038b29afc61c5b05853155a51
Erwanmanach <Manchoteu@aol.com>
2026-09-08 11:43:52 -0400

    remove unnessary file

--- src/virtual-robosub/utils/AUV_startup.m
+++
@@ -1,8 +0,0 @@
-clear;
-
-    load("thrustConfig.mat");
-    load("thruster_pwm.mat");
-    load("AUV.mat");
-    load("T200.mat");
-%Calculate Earth magnetic field and secular variation at a location using International Geomagnetic Reference Field
-[XYZ,H,D,I,F] = igrfmagm(1,33.7021,-117.7805,decyear(2024,7,4),13);
